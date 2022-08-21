38ac663f0a7127b3f57d97f462bc49d9eb53b552
bumpy09 <101124139+bumpy09@users.noreply.github.com>
2022-08-21 18:29:48 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+edubitRgbBit.setPixelColor(0, 0xff0000)
+edubitRgbBit.setPixelColor(1, 0x00ff00)
+edubitRgbBit.setPixelColor(2, 0xffff00)
+edubitRgbBit.setPixelColor(3, 0x0000ff)
+let Right_Color_and_Position = 0
+let RIght_Color_and_Wrong_Position = 0
+basic.forever(function () {
+	
+})
