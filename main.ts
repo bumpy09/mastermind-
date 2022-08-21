63b4d82e27424b6ffa79901519a26db0d845be1b
bumpy09 <101124139+bumpy09@users.noreply.github.com>
2022-08-21 19:07:35 +0800

    just now haven finish , now finish

--- main.ts
+++ main.ts
@@ -1,9 +1,22 @@
+input.onButtonPressed(Button.A, function () {
+    Right_Color_and_Position += 1
+    basic.showNumber(Right_Color_and_Position)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("A=")
+    basic.showNumber(Right_Color_and_Position)
+    basic.showString("B=")
+    basic.showNumber(RIght_Color_and_Wrong_Position)
+})
+input.onButtonPressed(Button.B, function () {
+    RIght_Color_and_Wrong_Position += 1
+    basic.showNumber(RIght_Color_and_Wrong_Position)
+})
+let RIght_Color_and_Wrong_Position = 0
+let Right_Color_and_Position = 0
 edubitRgbBit.setPixelColor(0, 0xff0000)
 edubitRgbBit.setPixelColor(1, 0x00ff00)
 edubitRgbBit.setPixelColor(2, 0xffff00)
 edubitRgbBit.setPixelColor(3, 0x0000ff)
-let Right_Color_and_Position = 0
-let RIght_Color_and_Wrong_Position = 0
-basic.forever(function () {
-	
-})
+Right_Color_and_Position = 0
+RIght_Color_and_Wrong_Position = 0
